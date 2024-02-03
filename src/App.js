05620af3651852c6ea19e513890aa536c2d1452e
Nishant <nishantdhamija41@gmail.com>
2024-02-03 23:18:18 +0530

    added Login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Free3UI from "./components/Free3UI/Free3UI";
 import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
 import CreateContract from "./components/CreateContract/CreateContract";
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Route path="/" element={<Free3UI />} />
       <Route path="/contract" element={<Free3UI />} />
       <Route path="/create" element={<CreateContract />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
