49090c602705b8d65d960655cfdb1dcf99423d04
Nishant <nishantdhamija41@gmail.com>
2024-02-04 23:09:38 +0530

    Added signUp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Free3UI from "./components/Free3UI/Free3UI";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import CreateContract from "./components/CreateContract/CreateContract";
+import SignUp from "./components/SignUp/SignUp";
 const App = () => {
   return (
     <Routes>
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/contract" element={<Free3UI />} />
       <Route path="/create" element={<CreateContract />} />
       <Route path="/login" element={<Login />} />
+      <Route path="signUp" element={<SignUp />} />
     </Routes>
   );
 };
